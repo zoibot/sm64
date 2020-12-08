0ad9edc01eb1a5b973950c7a342376955e2953dc
Matt Kempster <kidpixel@gmail.com>
2020-12-07 22:37:11 -0800

    [ddd] Make poles not appear on star 1

--- src/game/behaviors/ddd_pole.inc.c
+++ src/game/behaviors/ddd_pole.inc.c
@@ -1,6 +1,7 @@
 
 void bhv_ddd_pole_init(void) {
-    if (!(save_file_get_flags() & (SAVE_FLAG_HAVE_KEY_2 | SAVE_FLAG_UNLOCKED_UPSTAIRS_DOOR))) {
+    // if (!(save_file_get_flags() & (SAVE_FLAG_HAVE_KEY_2 | SAVE_FLAG_UNLOCKED_UPSTAIRS_DOOR))) {
+    if (gCurrActNum == 1) {
         mark_object_for_deletion(o);
     } else {
         o->hitboxDownOffset = 100.0f;
