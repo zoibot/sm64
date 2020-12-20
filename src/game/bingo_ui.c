e6542efb15190bf0ed5356f180e397e936e8c6fb
Matt Kempster <kidpixel@gmail.com>
2020-12-19 22:00:47 -0800

    [version] Bump version

--- src/game/bingo_ui.c
+++ src/game/bingo_ui.c
@@ -214,7 +214,7 @@ void draw_bingo_screen() {
     // Seed and time.
     sprintf(seed_print, "SEED %09d", gBingoInitialSeed);
     print_text_tiny(240, 214, seed_print);
-    print_text_tiny(240, 205, "VERSION 0.9a");
+    print_text_tiny(240, 205, "VERSION 0.9b");
     getTimeFmtPreciseTiny(timestamp, gbGlobalBingoTimer);
     sprintf(time_print, "TIME %s", timestamp);
     print_text_tiny(240, 196, time_print);
